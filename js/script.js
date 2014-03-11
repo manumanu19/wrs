be730f2ca5d53ac9001c0bbd2c49dcc0ab988cd7
manumanu19 <manumathews@hotmail.com>
2014-03-12 00:02:31 +0300

    Implement the title bar

--- js/script.js
+++ js/script.js
@@ -3,15 +3,15 @@
 	// Countdown Initiation and Settings
 
 	var deadlineDay = new Date();
-	deadlineDay = new Date(2014, 3 - 1, 11);
-	$('.lb-countdown').countdown({
+	deadlineDay = new Date(2014, 3 - 1, 17);
+	$('.wrs-countdown').countdown({
 		until: $.countdown.UTCDate(+0, deadlineDay), 
 		// timezone: +3, 
 		format: 'DHMS',
 		// layout: '<b>{d<}{dn} {dl} {d>}' + '{hn} {hl}, {mn} {ml}, {sn} {sl}</b>',
-		layout: '{d<}{dn} {sep} {d>}' + '{hn} {sep} {mnn} {sep} {snn}',
+		layout: '{d<}{dn}d {d>}' + '{hn} {sep} {mnn} . {snn}',
     	labels: [ 'Year', 'Mo', 'Wk', 'Days', 'Hr', 'Mn', 'Sc'],
-    	timeSeparator: '/',
+    	// timeSeparator: '/',
 	});
 
 	// Countdown Initiation and Settings
